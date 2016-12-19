f38e96c98421748796e9ed2e6c5b053fa1a5d089
Daniel Borland <DanielBorland@Daniels-MBP.hsd1.il.comcast.net>
2016-12-18 21:55:43 -0600

    js

--- repContactResources/repContactScripts.js
+++ repContactResources/repContactScripts.js
@@ -3,13 +3,12 @@ var queryResponse;
 
 //Google Civic Info API scripts
 
-function makeRequest(e) {
+function makeRequest() {
 	let addressToSearch = document.querySelector(".addressSearchBar");
 	let request = gapi.client.civicinfo.representatives.representativeInfoByAddress({ 'address': addressToSearch.value});
 	request.then(function(response) {
         queryResponse = response;
         console.log(queryResponse);
-        e.preventDefault();
 	});
 }
 
