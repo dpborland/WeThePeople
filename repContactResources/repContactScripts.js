4447d6abbddb401777df66e2ce28581d54db1b14
Daniel Borland <DanielBorland@Daniels-MBP.hsd1.il.comcast.net>
2017-04-16 20:41:33 -0500

    js

--- repContactResources/repContactScripts.js
+++ repContactResources/repContactScripts.js
@@ -15,6 +15,7 @@ function makeRequest(e) {
         window.location.href = "http://contactmyreps.com/results.html";
     }).then(function() {
     	let queryResponse1 = JSON.parse(localStorage.getItem("queryResponse"));
+    	console.log(queryResponse1);
     	document.addEventListener("DOMContentLoaded", constructResultsTemplate);
 	}).then(function() {
 		resultsTemplateFill();
