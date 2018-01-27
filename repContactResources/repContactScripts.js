294cd90391742af52e2eab742e4a2288a9c92ef0
Daniel Borland <DanielBorland@Daniels-MacBook-Pro.local>
2018-01-27 13:20:21 -0600

    added a link SVG to the links title, and small JS func to control color changes

--- repContactResources/repContactScripts.js
+++ repContactResources/repContactScripts.js
@@ -516,3 +516,25 @@ if (window.location.href === "http://contactmyreps.com/results.html") {
             });
     }, false);
 }
+
+if (window.location.href === "http://contactmyreps.com/links.html") {
+    const linkSVGs = Array.from(document.querySelectorAll(".linkSVGTitle"));
+    const titleContainers = Array.from(document.querySelectorAll(".linksTitle"));
+
+    titleContainers.forEach( (container, index) => {
+        container.addEventListener("mouseover", () => {
+            linkSVGs.forEach( (link) => {
+                link[index].style.backgroundColor = "rgba(0, 0, 99, 0.75)";
+            });
+        }, false);
+    });
+
+    titleContainers.forEach( (container, index) => {
+        container.addEventListener("mouseout", () => {
+            linkSVGs.forEach( (link) => {
+                link[index].style.backgroundColor = "rgba(148, 27, 56, 0.8)";
+            });
+        }, false);
+    });
+
+}
